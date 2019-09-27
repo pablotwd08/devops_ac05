9bd4259857b639ab7803563120d446b19f05c91b
julypalhares <juliana.palhares@aluno.faculdadeimpacta.com.br>
2019-09-26 22:14:09 -0300

    mano do ceu

--- com/bbjp/test/test_calcula_parcela.py
+++ com/bbjp/test/test_calcula_parcela.py
@@ -6,11 +6,11 @@ def test_valor0():
         assert valorPagamento(-1, 5) == None
 
 
-def test_valor():
+def test_valor2():
         assert valorPagamento(500, 50) == 765.0
 
 
-def test_valor():
+def test_valor1():
         assert valorPagamento(500, -1) == 500
 
 
