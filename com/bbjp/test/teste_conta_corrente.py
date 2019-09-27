7c888527464a9825987ab3a4e8d1f23b81a3ef6f
Igor Silva Santos <1901288@salas.aulas>
2019-09-26 22:25:52 -0300

    final 2.0

--- com/bbjp/test/teste_conta_corrente.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from com.bbjp.conta_corrente import ContaCorrente
-
-conta_correntee = ContaCorrente(1,'eu', 1.0)
-
-def alterarNome_test():
-    conta_correntee.alterarNome('tu')
-    assert conta_correntee.nomeCorrentista == 'tu'
