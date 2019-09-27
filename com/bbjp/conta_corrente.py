0d0d6c31e84c0197dbf5ba6d1d14f9799bce435f
Igor Silva Santos <1901288@salas.aulas>
2019-09-26 22:23:34 -0300

    final

--- com/bbjp/conta_corrente.py
+++ com/bbjp/conta_corrente.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class ContaCorrente:
 
     def __init__(self, numero, nomeCorrentista, saldo=0.0):
@@ -16,20 +15,3 @@ class ContaCorrente:
         self.saldo -= valor
 
 
-=======
-class ContaCorrente:
-
-    def __init__(self, numero, nomeCorrentista, saldo=0.0):
-        self.numero = numero
-        self.alterarNome(nomeCorrentista)
-        self.saldo = saldo
-
-    def alterarNome(self, nomeCorrentista):
-        self.nomeCorrentista = nomeCorrentista
-
-    def deposito(self, valor):
-        self.saldo += valor
-
-    def saque(self, valor):
-        self.saldo -= valor
->>>>>>> conta_corrente
