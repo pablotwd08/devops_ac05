adb10a27863719d706e6a00fdb0e1587bd7aee2d
Pablo Kalil Santana de Oliveira <1900997@SALAS.AULAS>
2019-09-26 21:36:53 -0300

    subindo codigo

--- com/bbjp/test/test_converte_hora.py
+++ com/bbjp/test/test_converte_hora.py
@@ -5,11 +5,11 @@ def test_converte_hora_none():
 	assert converteHora(24,70) == None, "should be none"
 
 def test_converte_hora_zero():
-	assert converteHora(0,22) == '00:22 AM', "should be 12"
+	assert converteHora(0,22) == '12:22 AM', "should be 12"
 
 def test_converte_hora_12():
 	assert converteHora(10,30) == "10:30 AM", "should be 10:30 AM"
 
 def test_converte_hora_24():
-	assert converteHora(13,13) == "13:13 PM", "should be 12"
+	assert converteHora(13,13) == "01:13 PM", "should be 12"
 
